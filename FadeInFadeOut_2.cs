708b4833436b4e0380fdc0d3519709a07343805e
SuHlaingHnin6318 <suhlaing.ucsy777@gmail.com>
2021-01-21 12:18:29 +0630

    Add FadeInFadeOut Script 2

---
+++ FadeInFadeOut_2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInFadeOut_2 : MonoBehaviour
+{
+    Image image;
+    Color c;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        c = image.color;
+        c.a = 0f;
+        image.color = c;
+
+        StartCoroutine("FadeIn");
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float f = 0.01f; f <= 1f; f += 0.01f)
+        {
+            c.a = f;
+            image.color = c;
+            yield return new WaitForSeconds(0.01f);
+        }
+        
+        StartCoroutine("FadeOut");
+    }
+
+    IEnumerator FadeOut()
+    {
+        for (float f = 1f; f >= 0; f -= 0.1f)
+        {
+            c.a = f;
+            image.color = c;
+            yield return new WaitForSeconds(0.1f);
+        }
+        StartCoroutine("FadeIn");
+    }
+}
